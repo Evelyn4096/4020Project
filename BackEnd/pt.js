9bf60c1c8a507a3b72c21d9605460d9bcd46995b
Evelyn <15037152690@163.com>
2025-11-26 01:29:27 -0500

    version5

--- BackEnd/pt.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const app=express();
-app.listen(3000,() => {console.log("EXpress run on http://localhost:3000")});
-app.set("view engine","ejs");
-app.set('views','FrontEnd_react');
-app.get('/',(req,res) => {
-    res.render("index", {title: 'home'})
-})
